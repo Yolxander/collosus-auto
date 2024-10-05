97f381a975a384035e17b20d7e281e12600ca592
YolxanderHC <yolxander.jaca@humancontact.com>
2024-10-04 20:52:08 -0400

    fixed errors 3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import { Roboto_Condensed } from 'next/font/google';
 import "./globals.css";
 
 const geistSans = localFont({
